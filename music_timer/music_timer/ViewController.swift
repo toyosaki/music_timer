a1882acf2c1e3574543ccbfcbf3bc04c7db1ffda
otyazuke <otyauke.tt@gmail.com>
2015-04-11 18:24:16 +0900

    no message

--- music_timer/music_timer/ViewController.swift
+++ music_timer/music_timer/ViewController.swift
@@ -8,41 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource {
 
+    var myUIPicker: UIPickerView = UIPickerView()
+    var minArray:NSArray = NSArray()
+    
+    myUIPciker.frame = CGRectMake(0,0,view.bounds.width,250.0)
+    myUIPicker.delegate = self
+    myUiPicker.dataSource = self
+    self.view.addSubview(myUIPicker)
+
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        //datePickerを作成する
-        var myDatePicker:UIDatePicker = UIDatePicker()
-        
-        //datePickerの設定
-        myDatePicker.datePickerMode = UIDatePickerMode.CountDownTimer
-        
-        
-        //値が変わった際のイベント
-        myDatePicker.addTarget(self,
-            action: Selector("onDidChangedData:"),
-            forControlEvents: UIControlEvents.ValueChanged)
-        
-        //DataPickerをViewに追加
-        self.view.addSubview(myDatePicker)
-        
-
-    }
-    
-    @IBOutlet weak var myTextField: UILabel!
-    
-    //datePickerが変更された時に呼ばれる
-    func onDidChangedData(sender:NSObject)
-    {
-        let myDateFormatter: NSDateFormatter = NSDateFormatter()
-        myDateFormatter.dateFormat = "hh:mm"
-        var mySelectedData: NSString = myDateFormatter.stringFromDate(sender.date)
-        myTextField.text = mySelectedData
     }
     
 
