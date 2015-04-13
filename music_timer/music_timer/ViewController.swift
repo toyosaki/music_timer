d93fcccc2c22a273025310ccf8352014f25f1248
otyazuke <tsuruda_takuya@Takuya-no-MacBook-Pro.local>
2015-04-13 11:08:04 +0900

    no message

--- music_timer/music_timer/ViewController.swift
+++ music_timer/music_timer/ViewController.swift
@@ -8,23 +8,64 @@
 
 import UIKit
 
+
 class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource {
 
+    func makeArray(inout array:[Int]) {
+        for var i = 0 ; i<60 ; i++ {
+            array[i] = i+1
+        }
+    }
+    
     var myUIPicker: UIPickerView = UIPickerView()
-    var minArray:NSArray = NSArray()
     
-    myUIPciker.frame = CGRectMake(0,0,view.bounds.width,250.0)
-    myUIPicker.delegate = self
-    myUiPicker.dataSource = self
-    self.view.addSubview(myUIPicker)
-
-    
-    
-    
+    var minArray:NSArray = ["1"]
+    makeArray(&minArray)
+    var secArray:NSArray = ["1","2","3","4","5","6","7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myUIPicker.frame = CGRectMake(0,0,view.bounds.width,250.0)
+        myUIPicker.delegate = self
+        myUIPicker.dataSource = self
+        self.view.addSubview(myUIPicker)
+    }
+    
+    //表示例
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2 
+    }
+    
+    //表示個数
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if(component == 0){
+            return minArray.count
+        }else if(component == 1){
+            return secArray.count
+        }
+        return 0;
+    }
+    
+    //表示内容
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        if(component == 0){
+            return minArray[row] as String
+        }else if(component == 1){
+            return secArray[row] as String
+        }
+        return "";
+    }
+    
+    //選択時
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if(component == 0){
+            println("列\(row)")
+            println("値\(minArray[row])")
+        }else if(component == 1){
+            println("列\(row)")
+            println("値\(secArray[row])")
+        }
     }
     
 
